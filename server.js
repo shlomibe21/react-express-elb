d19df9364688def09d931c1b59b711dfc26c68fd
shlomibe <shlomi@onca-inc.com>
2022-02-14 23:00:07 -0800

    Build server and add .ebextensions

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // server.js
 //
-const express = require('express');
+import path from 'path';
+import express from 'express';
+
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('/', (req, res) => {
   res.send('just gonna send it');
 });
+
 app.get('/flower', (req, res) => {
   res.json({
     name: 'Dandelion',
